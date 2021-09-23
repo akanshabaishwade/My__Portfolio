dc45f7be03a75a147d4ff05edf59d85849998b42
akansha baishwade <akanshabaishwade@gmial.com>
2021-09-24 00:17:13 +0530

    update

--- iPortfolio/assets/js/main.js
+++ iPortfolio/assets/js/main.js
@@ -255,3 +255,17 @@
   });
 
 })()
+$(document).ready(function(){
+  $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+
+    $(".zoom").hover(function(){
+
+		$(this).addClass('transition');
+	}, function(){
+
+		$(this).removeClass('transition');
+	});
+});
